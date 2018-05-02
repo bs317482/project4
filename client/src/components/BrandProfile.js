1ea411ee02354ea36e2302840ed7ec96a2614cf3
bs317482 <bernardbnard@gmail.com>
2018-05-02 15:33:03 -0400

    minor css

--- client/src/components/BrandProfile.js
+++ client/src/components/BrandProfile.js
@@ -6,8 +6,7 @@ import axios from 'axios'
 class BrandProfile extends Component {
     state = {
         brand: {},
-        models: [],
-        features: []
+        models: []
     }
     componentDidMount() {
         console.log('Lamborghini')
@@ -17,13 +16,28 @@ class BrandProfile extends Component {
                 this.setState({
                     brand: response.data,
                     models: response.data.models
+                    
                 })
             })
     }
     render() {
         const modeler = this.state.models.map((model) => {
-            return <div>{model.name}</div>
+            console.log("Model features", model.features)
+            const features = model.features.map((feature) => {
+                return (
+                    <div>
+                        <div>{feature.title}</div>
+                        <div>{feature.description}</div>
+                    </div>)
+            })
+            return (
+                <div>
+                    <div>{model.name}</div>
+                    {features}
+                </div>
+            )
         })
+
 return (
             <div>
                 <p>{this.state.brand.name}</p>
--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const ShowContainer = styled.div`
+background-color: rgb(21, 141, 165);
+border: 5px solid black;
+height: 32.5vh;
+width: 100vw;
+font-size:40px;
+` 
 
 class Show extends Component {
     state = {
@@ -22,11 +31,13 @@ class Show extends Component {
     console.log("rendered")
     const brander =this.state.brands.map ((brand) => {
     return (
+      <ShowContainer>
       <div>
       <Link to={`/show/brand/${brand._id}`}>
       <p>{brand.name}</p>
       </Link>
       </div>
+      </ShowContainer>
     )
   })
   return brander
